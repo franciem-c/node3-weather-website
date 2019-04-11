bfa78386c4a638bcf4498f964a0eb1f5633df37f
Francie McKee-Clabaugh <sierra_fmc@hotmail.com>
2019-04-11 11:49:39 -0700

    Add high and low to forecast and add degree symbol

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.  There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + '°F outside. The high today is ' + body.daily.data[0].temperatureHigh + '°F with the low of ' + body.daily.data[0].temperatureLow + '°F. There is a ' + body.daily.data[0].precipProbability * 100 + '% chance of rain today.')
         }
     })
 }
